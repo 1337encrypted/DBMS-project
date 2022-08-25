1062733516ad9c0c2a02d98c334fedd6771cb1a0
shradhapatil22 <shradhapatil2202@gmail.com>
2022-08-25 23:05:07 +0530

    Edited doctor and supplier

--- Pharmacy GUI/supplier.py
+++ Pharmacy GUI/supplier.py
@@ -31,3 +31,44 @@ class supplier:
         self.address = address
         self.name = name
         self.licence = licence
+
+        # create table
+        def createCustomer(self):
+            query = 'create table if not exists customer(address varchar(40),name varchar(20),licence varchar(20), primary key licence)'
+            # cur = self.con.cursor()
+            cur.execute(query)
+            self.con.commit()
+            print("\nSuccessfull")
+
+        # insert
+        def insertCustomer(self, name, ph_no, address, customer_id, gender, employee_id):
+            query = "insert into customer(address ,name ,licence ) values('{}','{}','{}')"
+            # cur = self.con.cursor()
+            cur.execute(query)
+            self.con.commmit()
+
+        # fetch
+        def fetchAll(self):
+            query = "select * from customer"
+            # cur = self.con.cursor()
+            cur.execute(query);
+            for row in cur:
+                print(row)
+
+        # delete
+        def deleteCustomer(self, customer_id):
+            query = "delete from supplier where licence = {} ".format(licence)
+            # cur = self.con.cursor()
+            cur.execute(query)
+            self.con.commit()
+            print("Delete");
+
+        # update
+        def updateCustomer(self, address, name,   licence):
+            query = "update doctor set name='{}',age={},phone='{}',clinicAddress='{}',licence='{}' where licence = ".format(
+                address,name,  licence)
+            print(query)
+            # cur = self.con.cursor()
+            cur.execute(query)
+            self.con.commit()
+            print("updated");
